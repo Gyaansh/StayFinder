c5a6788e24d16ab59fefe3269432d59d123ae524
Gyaansh <gyaansh70@gmail.com>
2026-04-25 19:52:42 +0530

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
  server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://findstay-29ni.onrender.com",
         changeOrigin: true,
         secure: false,
       },
