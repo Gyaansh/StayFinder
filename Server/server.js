0bad785e864106e2ae642adf5a3245ff3a3d2957
Gyaansh <gyaansh70@gmail.com>
2026-04-25 20:14:04 +0530

    fix: removed import@tailwind in vite.config

--- Server/server.js
+++ Server/server.js
@@ -28,10 +28,7 @@ const app = express();
 const port = 8080;
 
 DbConnect();
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use("/uploads", express.static("Uploads"));
