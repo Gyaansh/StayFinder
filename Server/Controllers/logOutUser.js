4b846c1f56561e09f7e50969667100e1591d1af4
Gyaansh <gyaansh70@gmail.com>
2026-04-26 19:52:19 +0530

    fix: login and logout

--- Server/Controllers/logOutUser.js
+++ Server/Controllers/logOutUser.js
@@ -1,8 +1,8 @@
 function logOutUser(req,res){
     res.clearCookie("token",{
       httpOnly: true,        // cannot be accessed by JS
-      secure: false,         // true in production (HTTPS)
-      sameSite: "strict",
+      secure: true,         // true in production (HTTPS)
+      sameSite: "none",    //  none in production
     });
 
     res.status(200).json({
